6330a00ce6f16d961658b7039a7e809f4e54e4c2
santoshpremi <santoshadhikaripremi@gmail.com>
2025-03-25 10:52:18 +0100

    deployment

--- hydro/settings.py
+++ hydro/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+ALLOWED_HOSTS = ['hydrolevel-monitor.onrender.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +124,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "maps/static",
 ]
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
