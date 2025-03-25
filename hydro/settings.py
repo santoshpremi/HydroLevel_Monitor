6a78ec612a7f57dc6ab838b4dbfe7cd15d0cf8d3
santoshpremi <santoshadhikaripremi@gmail.com>
2025-03-25 10:57:30 +0100

    dep-allow

--- hydro/settings.py
+++ hydro/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = ['*']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
